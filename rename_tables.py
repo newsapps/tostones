e1bd1a2b484951fb7e3ff9e279674e7e3268f544
cgroskopf <cgroskopf@tribune.com>
2010-03-11 17:14:30 -0600

    Added missed file.

---
+++ rename_tables.py
@@ -0,0 +1,9 @@
+#!/bin/env python
+
+# TODO
+# Walk out/tables
+# Kill index.html
+# Iterate over every directory not "images", "javascripts", or "stylesheets"
+# Rename index.html to the folder name
+# Move example.html to the out/tables
+# Remove folders
